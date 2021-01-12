3f9ff355519f2512ab0600cc0442c099577d9b8f
20161313 <dbstjd427@gmail.com>
2021-01-12 16:55:34 +0900

    #8강_배열다루기(2)제거와 수정

--- first_react/src/App.js
+++ first_react/src/App.js
@@ -5,8 +5,7 @@ import MyName from './components/MyName';
 //import Counter from './components/Counter';
 //import Problematic from './components/Problematic';
 import PhoneForm from './components/PhoneFrom';
-
-
+import PhoneInfoList from './components/PhoneInfoList';
 class App extends Component {
   id = 2;
   state = {
@@ -25,6 +24,23 @@ class App extends Component {
       information: information.concat({ id: this.id++, ...data })
     })
   }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+          ? { ...info, ...data } // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
+          : info // 기존의 값을 그대로 유지
+      )
+    })
+  }
   render () {
     const { information } = this.state;
     return (
@@ -35,7 +51,9 @@ class App extends Component {
         <PhoneForm
           onCreate={this.handleCreate}
         />
-        {JSON.stringify(information)}
+        <PhoneInfoList data={information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate} />
       </div>
     );
   }
