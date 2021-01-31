cb911dfdea5a0f7945364cbc2724f26999d1042a
yooncastle <dbstjd427@gmail.com>
2021-01-31 23:29:43 +0900

    Immutable.js
    3. 변화를 일으키는 함수까지

--- first_react/src/index.js
+++ first_react/src/index.js
@@ -59,6 +59,54 @@ import reportWebVitals from './reportWebVitals';
 // nextArr = nextArr.delete(0);
 // console.log(nextArr.toJS());
 
+
+// Immutable.js의 .get 이나 .getIn 대신 Record 사용
+
+// import { Record } from 'immutable';
+// const Person = Record({
+//   name: 'YoonCastle',
+//   age: 17
+// });
+
+// let person = Person();
+// console.log(person);
+// console.log(person.name, person.age);
+// person = person.set('name', 'Chaewon');
+// console.log(person.name);
+// // 이건 오류 납니다: person.name = '철수';
+
+// // Record 에서 사전 준비해주지 않은 값을 넣어도 오류납니다.
+// // person = person.set('job', 5);
+
+// // 값을 따로 지정해줄수도 있습니다.
+// person = Person({
+//   name: 'Yena',
+//   age: 10
+// });
+
+// const { name, age } = person; // 비구조화 할당도 문제없죠.
+// console.log(name, age); // "영희" 10
+
+// // 재생성 할 일이 없다면 이렇게 해도 됩니다.
+// const dog = Record({
+//   name: '멍멍이',
+//   age: 1
+// })()
+
+// console.log(dog.name); // 멍멍이
+
+// // 이런것도 가능하죠.
+// const nested = Record({
+//   foo: Record({
+//     bar: true
+//   })()
+// })();
+
+// // Map 다루듯이 똑같이 쓰면 됩니다.
+// const nextNested = nested.setIn(['foo', 'bar'], false);
+// console.log(nextNested);
+// console.log(nested.foo.bar); // true
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
