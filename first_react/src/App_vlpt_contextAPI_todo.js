cf0839a88965412f7dc84e02de9d4ca341311ab8
yooncastle <dbstjd427@gmail.com>
2021-08-29 17:57:25 +0900

    vlpt_contextAPI_todoList  기능 구현

--- first_react/src/App_vlpt_contextAPI_todo.js
+++ first_react/src/App_vlpt_contextAPI_todo.js
@@ -4,6 +4,7 @@ import TodoTemplate from './Components/vlptcontextAPItodoComp/TodoTemplate';
 import TodoHead from './Components/vlptcontextAPItodoComp/TodoHead';
 import TodoList from './Components/vlptcontextAPItodoComp/TodoList';
 import TodoCreate from './Components/vlptcontextAPItodoComp/TodoCreate';
+import { TodoProvider } from './Components/vlptcontextAPItodoComp/TodoContext';
 const GlobalStyle = createGlobalStyle`
   body {
     background: #e9ecef;
@@ -12,14 +13,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App () {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
